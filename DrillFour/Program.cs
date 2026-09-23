255723c6b32fbd945f05be0afd4de918d8b8934f
Aniya Nicole Corivano <aniyacorivano@webdev09.local>
2026-09-23 12:30:38 -0400

    committed

---
+++ DrillFour/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int secret = 7;
+        int attemptsLeft = 3;
+        bool guessedCorrectly = false;
+
+        Console.Write("Guess the secret number: ");
+
+        // TODO: Set up a while loop that runs while attemptsLeft > 0 AND guessedCorrectly is false
+        // 1. Prompt user for guess
+        // 2. If correct, set guessedCorrectly to true
+        // 3. If wrong, decrement attemptsLeft and print remaining attempts
+
+        while (attemptsLeft > 0 && guessedCorrectly = false)
+        {
+            Console.Write("Guess the secret number: ");
+            attemptsLeft --;
+        }
+
+        if (guessedCorrectly = true)
+            Console.WriteLine("You Win!");
+        else
+            Console.WriteLine("Out of attempts! You lose.");
+    }
+}
